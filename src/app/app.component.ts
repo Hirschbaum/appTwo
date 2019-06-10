1b7b5bb21c4a4ee30a8d6e9550f8b04dd25564d3
Adrienn Timko <adrienn.timko@gmail.com>
2019-06-10 15:56:27 +0200

    angular exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appTwo';
+  title = 'the Jungle';
+  lineOne = 'Jungle, welcome to the jungle';
+  lastLine = "Yeah, Guns N' Roses... not my music";
+  nextLines = [
+    {line: 'Watch it bring you to your '},
+    {line: 'shun n-n-n-n-n-n-n-n knees, knees'},
+    {line: 'Uh, I, I want to watch you bleed...'},
+    ];
 }
